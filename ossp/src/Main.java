dab0eaa83964fadd9b648b33539242040a073a4c
MINJU <alswn9938@gmail.com>
2022-09-22 14:34:39 +0900

    2022-09-22 실습과제#4 3번

---
+++ ossp/src/Main.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("이름을 입력하세요: ");
+        String name = br.readLine();
+
+        System.out.print("학번을 입력하세요: ");
+        String student_id = br.readLine();
+
+        System.out.println();
+
+        System.out.println("<출력>");
+        System.out.println("이름: " + name);
+        System.out.println("학번: " + student_id);
+    }
+}
